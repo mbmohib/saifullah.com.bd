c2886f16e08e0cb9b2960d3e13b30e45ad724c7e
Mohib <mbmohib@gmail.com>
2021-03-05 15:47:42 +0600

    checking style component performance

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,7 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import styled from "styled-components";
 import { Header, Footer, SEO } from "./";
-import { GlobalStyle, theme } from "../styles";
-import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles";
 
 const LayoutWrapper = styled.div`
   background: var(--bg-color);
@@ -31,15 +30,13 @@ export default function Layout({ title, children }) {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <SEO title={title || data.site.siteMetadata.title} />
-        <LayoutWrapper>
-          <Header />
-          <Main>{children}</Main>
-          <Footer />
-        </LayoutWrapper>
-      </ThemeProvider>
+      <GlobalStyle />
+      <SEO title={title || data.site.siteMetadata.title} />
+      <LayoutWrapper>
+        <Header />
+        <Main>{children}</Main>
+        <Footer />
+      </LayoutWrapper>
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,17 +15,17 @@ const Container = styled(Box)`
   padding: 32px;
   transition: all ease-in-out 0.2s;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.4);
-  background: ${({ theme }) =>
-      theme.name === "dark" ? theme.colors.dark1 : theme.colors.light2}
+  /* background: ${({ theme }) =>
+    theme.name === "dark" ? theme.colors.dark1 : theme.colors.light2}
     linear-gradient(
       -45deg,
       ${({ theme, language }) =>
-          language && theme.colors[language]
-            ? theme.colors[language]
-            : theme.colors.primary3}
+    language && theme.colors[language]
+      ? theme.colors[language]
+      : theme.colors.primary3}
         16px,
       rgba(0, 0, 0, 0.01) 0
-    );
+    ); */
 
   &:hover {
     transform: translateY(-4px);
