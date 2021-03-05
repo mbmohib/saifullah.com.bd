a8e4183c48dd17429da7b83d4b0bd347397cca68
Mohib <mbmohib@gmail.com>
2021-03-05 15:04:13 +0600

    checking style component performance

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 24px;
+  color: ${({ color }) => color};
+`;
+
+export default Button;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Layout, Text, Box } from "../components";
+import { Layout, Text, Box, Button } from "../components";
 
 export default function IndexPage() {
   return (
@@ -7,6 +7,7 @@ export default function IndexPage() {
       <Box mt={4}>
         <p>Hello from layout</p>
         <Text>Hello from Text</Text>
+        <Button color="red">Click</Button>
       </Box>
     </Layout>
   );
