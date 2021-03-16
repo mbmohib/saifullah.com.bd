6bfbf863cd80372e1ffbd34dfe4e3dea27a4fe48
Mohib <mbmohib@gmail.com>
2021-03-16 13:29:02 +0600

    Updates

--- src/hooks/index.js
+++
@@ -1,1 +0,0 @@
-export { default as useBackgroundImage } from "./useBackgroundImage";
--- src/icons/index.js
+++
@@ -1,9 +0,0 @@
-export { default as RightArrow } from "./RightArrow";
-export { default as DownArrowSmall } from "./DownArrowSmall";
-export { default as Facebook } from "./Facebook";
-export { default as Youtube } from "./Youtube";
-export { default as Phone } from "./Phone";
-export { default as Email } from "./Email";
-export { default as Video } from "./Video";
-export { default as Book } from "./Book";
-export { default as Logo } from "./Logo";
