bbc50422c871e0429a8d34aab837f200aed40dd2
Mohib <mbmohib@gmail.com>
2021-03-19 15:21:28 +0600

    remove styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: "Abdul Hi Muhammad Saifullah",
+    title: 'Abdul Hi Muhammad Saifullah',
     description: `Abdul Hi Muhammad Saifullah, Religious Consultant at E.B solutions Limited & Khatib at Masjidul Jumawa Complex`,
     author: `monkef.com`,
-    siteUrl: "https://saifullah.com.bd"
+    siteUrl: 'https://saifullah.com.bd',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png"
-      }
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: `${__dirname}/src/images/static`
+        icon: 'src/images/icon.png',
       },
-      __key: "images"
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/static`,
+      },
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "background_images",
-        path: `${__dirname}/src/images/background`
+        name: 'background_images',
+        path: `${__dirname}/src/images/background`,
       },
-      __key: "background_images"
-    }
-  ]
+      __key: 'background_images',
+    },
+  ],
 };
